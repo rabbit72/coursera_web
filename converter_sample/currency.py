cca4137f8604b9843726e95412a4ef727b9ba41c
Daniil Shadrin <rabbit72rus@gmail.com>
2018-08-31 16:31:20 +0300

    init

---
+++ converter_sample/currency.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from decimal import Decimal
+
+
+def convert(amount, cur_from, cur_to, date, requests):
+    url = "http://www.cbr.ru/scripts/XML_daily.asp"
+    params = {"date_req": date}
+    response = requests.get(url, params)  # Использовать переданный requests
+    soup = BeautifulSoup(response.text, "lxml")
+    currencies = soup.find_all("valute")
+
+    _from = get_exchange_info(cur_from, currencies)
+    _to = get_exchange_info(cur_to, currencies)
+    result = round(amount * _from / _to, 4)
+    return result
+
+
+def get_exchange_info(name_currency, currencies):
+    if name_currency == "RUR":
+        return 1
+    for currency in currencies:
+        if currency.charcode.text == name_currency:
+            nominal = int(currency.nominal.text)
+            value = Decimal(currency.value.text.replace(",", "."))
+            return value / nominal
